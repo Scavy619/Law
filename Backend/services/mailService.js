a930052e2c7fe1b234b1638fba1cffed3aea3a0b
Divyansh <officialdslc1552005@gmail.com>
2026-03-04 00:10:32 +0530

    Email service updated to brevo instead of nodemailer+googlesmtp

--- Backend/services/mailService.js
+++ Backend/services/mailService.js
@@ -1,22 +1,44 @@
-import nodemailer from 'nodemailer';
+// import nodemailer from 'nodemailer';
 
-const transporter = nodemailer.createTransport({
-  host: process.env.EMAIL_HOST,
-  port: process.env.EMAIL_PORT,
-  secure: false,
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
-  },
-});
+// const transporter = nodemailer.createTransport({
+//   host: process.env.EMAIL_HOST,
+//   port: process.env.EMAIL_PORT,
+//   secure: false,
+//   auth: {
+//     user: process.env.EMAIL_USER,
+//     pass: process.env.EMAIL_PASS,
+//   },
+// });
 
+// export async function sendEmail({to, subject, html}){
+//   await transporter.sendMail({
+//     from: process.env.EMAIL_USER,
+//     to,
+//     subject,
+//     html,
+//   });
+// }
 
-export async function sendEmail({to, subject, html}){
-  await transporter.sendMail({
-    from: process.env.EMAIL_USER,
-    to,
-    subject,
-    html,
-  });
-}
+// using brevo now since we gotta host it and render blocks smtp
+import { BrevoClient } from "@getbrevo/brevo";
 
+const client = new BrevoClient({ apiKey: process.env.BREVO_API_KEY });
+
+export const sendEmail = async ({ to, subject, html }) => {
+  try {
+    const response = await client.transactionalEmails.sendTransacEmail({
+      sender: {
+        name: "LawBridge",
+        email: "lawbridgeorg@gmail.com",
+      },
+      to: [{ email: to }],
+      subject,
+      htmlContent: html,
+    });
+
+    return response;
+  } catch (error) {
+    console.error("Brevo email error:", error);
+    throw new Error("Email sending failed");
+  }
+};

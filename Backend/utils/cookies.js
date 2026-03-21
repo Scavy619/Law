fd2f2f5a826fc7275965cd02088897d0d2e41eb3
Divyansh <officialdslc1552005@gmail.com>
2026-03-21 23:03:25 +0530

    one more try

--- Backend/utils/cookies.js
+++ Backend/utils/cookies.js
@@ -1,7 +1,7 @@
 export const refreshCookieOptions = {
   httpOnly: true,
-  secure: process.env.NODE_ENV === "production",
-  sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+  secure: true,
+  sameSite: "none",
   path: "/",
   maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
 };

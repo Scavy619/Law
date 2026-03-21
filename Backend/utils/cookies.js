f34918b895daa165141b5b61cf79cfcb8d66fb04
Divyansh <officialdslc1552005@gmail.com>
2026-03-21 22:58:47 +0530

    some fixes

--- Backend/utils/cookies.js
+++ Backend/utils/cookies.js
@@ -6,7 +6,6 @@ export const refreshCookieOptions = {
   maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
 };
 
-
-// since i am doing sameSite=none becoz frontend will be on vercel, backend on render i cannot do lax, strict islie mujhe refresh token ko csrf 
+// since i am doing sameSite=none becoz frontend will be on vercel, backend on render i cannot do lax, strict islie mujhe refresh token ko csrf
 // se bachane k liye, request kaha se aa rahi hai ie which origin ye validate krna pdega varna they can do csrf
 // so see validate origin thang in utils
